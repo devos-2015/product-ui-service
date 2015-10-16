bf5242b0015bd66ca0b60ce7c3182c2dce47e6c4
Robin <robin-genske@web.de>
2015-10-16 15:59:55 +0200

    Connected to live service lol rofl impl staff

--- UI/index.js
+++ UI/index.js
@@ -2,21 +2,50 @@ $(function () {
     function ProductViewModel(product){
         this.album = ko.observable(product.album);
         this.interpret = ko.observable(product.interpret);
-        this.preis = ko.observable(product.preis);
+        this.price = ko.observable(product.price);
+        this.id = product.id;
     }
 
     function AppViewModel() {
         this.products = ko.observableArray();
         var self = this;
-        this.add = function(product){
-            self.products.push(product);
-            self.newProduct(new ProductViewModel({}));
-        };
-        this.save = function(product){
 
+        this.add = function(product){
+            $.post(
+            '/products',
+            {
+              album : product.album(),
+              price : product.price(),
+              interpret : product.interpret()
+            },
+            function(id){
+              product.id = id;
+              self.products.push(product);
+              self.newProduct(new ProductViewModel({}));
+            })
         };
+
+        this.save = function(product){
+          $.ajax(
+            '/products/' + product.id,
+            {
+              method : 'PUT',
+              data: {
+                album : product.album(),
+                price : product.price(),
+                interpret : product.interpret()
+              }
+            });
+        };
+
         this.remove = function(product){
-            self.products.remove(product);
+          $.ajax(
+            '/products/' + product.id,
+            {
+              method : 'DELETE'
+            }
+          ).done(() => self.products.remove(product));
+
         };
         this.newProduct = ko.observable(new ProductViewModel({}));
     }
--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 var fs = require('fs');
 var restify = require('restify');
+var sdk = require('lc-sdk-node.js');
+
+var client = sdk({
+  discoveryServers : [
+    '46.101.245.190:8500',
+    '46.101.132.55:8500',
+    '46.101.193.82:8500'
+    ]
+});
 
 var SERVICE_CHECK_HTTP = process.env.SERVICE_CHECK_HTTP || '/healthcheck';
 var PORT = process.env.PORT || 3000;
 
 var app = restify.createServer();
+app.use(restify.queryParser());
+app.use(restify.bodyParser());
+
+app.get(SERVICE_CHECK_HTTP, function(req, res, next){
+    res.send(200,{message : 'ok'});
+});
 
 app.get('/', restify.serveStatic({
     directory: __dirname + '/UI',
@@ -16,24 +31,21 @@ app.get('/UI/index.js', restify.serveStatic({
     file: 'index.js'
 }));
 
-app.get(SERVICE_CHECK_HTTP, function(req, res, next){
-    res.send(200,{message : 'ok'});
-});
-
-
 app.get('/products', function (req, res, next) {
-    res.send([
-        {
-            album: "Karaoke Fun",
-            preis: -1.00,
-            interpret: "Wolfgang Petri"
-        },
-        {
-            album: "Sonic Highway",
-            preis: 3.99,
-            interpret: "Foo Fighters"
-        }
-    ]);
+  client.get('product-service', '/Products').then(data => res.send(200, data).);
 });
+
+app.del('/products/:id', function(req, res, next){
+  client.del('product-service','/Products' + req.params.id);
+})
+
+app.put('/products/:id', function(req, res, next){
+  client.put('product-service', '/Products'+ req.params.id ,request.body);
+})
+
+app.post('/products', function(req, res, next){
+  client.put('product-service', '/Products',request.body).then(data => res.send(201, data));
+})
+
 
 app.listen(PORT || 3000);
